0272848eab15b68fa6d52fa45647de1353bf1334
tamim-18 <tamim11903060@gmail.com>
2024-07-02 17:45:22 +0600

    userModel created

---
+++ models/productModel.ts
@@ -0,0 +1,101 @@
+// creating product model
+
+import mongoose, { Schema } from "mongoose";
+import { ProductModelTypes } from "../types/productModelTypes";
+import { ObjectId } from "mongodb";
+
+const productSchema = new Schema<ProductModelTypes>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    // category is an array of objectIds. Each objectId is a category
+    category: [
+      {
+        type: ObjectId,
+        ref: "Category",
+      },
+    ],
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    //color is an array of strings. Each string is a color
+    color: {
+      type: String,
+      enum: [
+        "white",
+        "black",
+        "blue",
+        "red",
+        "green",
+        "yellow",
+        "brown",
+        "grey",
+        "purple",
+        "pink",
+      ],
+    },
+    brand: {
+      type: String,
+      enum: [
+        "Dell",
+        "Hp",
+        "Lenovo",
+        "Asus",
+        "Acer",
+        "Apple",
+        "Samsung",
+        "Sony",
+        "Microsoft",
+        "Toshiba",
+        "IBM",
+        "Others",
+      ],
+    },
+    images: {
+      type: Array,
+    },
+    // ratings are stored in an array of objects. Each object contains the star rating, comment, and the user who posted the rating
+    ratings: [
+      {
+        star: {
+          type: Number,
+        },
+        comment: {
+          type: String,
+        },
+        postedby: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+  },
+  //timestamps
+  {
+    timestamps: true,
+  }
+);
+
+////Export the model
+export default mongoose.model<ProductModelTypes>("Product", productSchema);
---
+++ types/productModelTypes.ts
@@ -0,0 +1,22 @@
+import { Types } from "mongoose";
+
+// interface of the product model
+export interface ProductModelTypes {
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  category: Types.ObjectId[];
+  brand: string;
+  quantity: number;
+  sold?: number;
+  images?: Array<{ public_id: string; url: string }>;
+  color?: string[];
+  tags?: string;
+  ratings?: Array<{
+    star: number;
+    comment: string;
+    postedby: Types.ObjectId;
+  }>;
+  totalrating?: string;
+}
