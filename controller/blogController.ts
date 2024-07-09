78d8ac8d046f1c04bcf50d53b54f793a59a512a2
tamim-18 <tamim11903060@gmail.com>
2024-07-09 22:08:16 +0600

    buggy

--- controller/blogController.ts
+++ controller/blogController.ts
@@ -2,6 +2,8 @@ import createHttpError from "http-errors";
 import blogModel from "../models/blogModel";
 import userModel from "../models/userModel";
 import e, { Request, Response, NextFunction } from "express";
+import { AuthRequest } from "../middlewares/authenticate";
+import validateMongoDbId from "../utils/validateMongoDbId";
 
 const createBlog = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -24,5 +26,92 @@ const updateAblog = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(500, "Failed to update the blog"));
   }
 };
+// get a blog
 
-export { createBlog, updateAblog };
+const getAblog = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const blog = await blogModel.findById(id);
+    const updateViews = await blogModel.findByIdAndUpdate(
+      id,
+      {
+        $inc: { numViews: 1 },
+      },
+      { new: true }
+    );
+    res.json(updateViews);
+  } catch (err) {
+    return next(createHttpError(500, "Failed to get the blog"));
+  }
+};
+// delete a blog
+const deleteAblog = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const blog = await blogModel.findByIdAndDelete(id);
+    res.json(blog);
+  } catch (err) {
+    return next(createHttpError(500, "Failed to delete the blog"));
+  }
+};
+
+// get all blogs
+const getAllBlogs = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const blogs = await blogModel.find();
+    res.json(blogs);
+  } catch (err) {
+    return next(createHttpError(500, "Failed to get all blogs"));
+  }
+};
+//like a blog
+const likeBlog = async (req: Request, res: Response, next: NextFunction) => {
+  //get the login user
+  const _req = req as AuthRequest;
+  console.log(_req.userId);
+  // get the blog id
+  const { blogId } = req.body;
+  console.log(blogId);
+  try {
+    //find the blog
+    const blog = await blogModel.findById(blogId);
+    console.log(blog);
+    const isLiked = blog?.isLiked;
+    const alreadyDisliked = blog?.dislikes?.find(
+      (dislike) => dislike.toString() === _req.userId
+    ); //check if the user has already disliked the blog
+    if (alreadyDisliked) {
+      const updatedBlog = await blogModel.findByIdAndUpdate(blogId, {
+        $pull: { dislikes: _req.userId }, //remove the user from the dislikes array
+        isDisliked: false,
+      });
+    }
+    if (isLiked) {
+      const updatedBlog = await blogModel.findByIdAndUpdate(blogId, {
+        $pull: { likes: _req.userId }, //remove the user from the likes array
+        isLiked: false,
+      });
+    } else {
+      const updatedBlog = await blogModel.findByIdAndUpdate(
+        blogId,
+        {
+          $push: { likes: _req.userId }, //add the user to the likes array
+          isLiked: true,
+        },
+        { new: true }
+      );
+      res.json(updatedBlog);
+    }
+  } catch (err) {
+    return next(createHttpError(500, "Failed to like the blog"));
+  }
+};
+
+export {
+  createBlog,
+  updateAblog,
+  getAblog,
+  deleteAblog,
+  getAllBlogs,
+  likeBlog,
+};
--- routes/blogRouter.ts
+++ routes/blogRouter.ts
@@ -1,12 +1,28 @@
 //blog router
 import express from "express";
 import { authentication, isAdmin } from "../middlewares/authenticate";
-import { createBlog, updateAblog } from "../controller/blogController";
+import {
+  createBlog,
+  deleteAblog,
+  getAblog,
+  getAllBlogs,
+  likeBlog,
+  updateAblog,
+} from "../controller/blogController";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/", authentication, isAdmin, createBlog);
+
 //update a blog
 blogRouter.put("/:id", authentication, isAdmin, updateAblog);
+//get a blog
+blogRouter.get("/:id", getAblog);
+//delete a blog
+blogRouter.delete("/:id", authentication, isAdmin, deleteAblog);
+//get all blogs
+blogRouter.get("/", getAllBlogs);
+//like a blog
+blogRouter.put("/likes", authentication, likeBlog);
 
 export default blogRouter;
