11362e1f3389a8af1e6ae41a28ce4709bf92ccd3
tamim-18 <tamim11903060@gmail.com>
2024-07-29 20:02:59 +0600

    config cloudinary

--- config/config.ts
+++ config/config.ts
@@ -10,6 +10,9 @@ const _cofig = {
   jwtSecret: env.JWT_SECRET_KEY,
   email: env.EMAIL,
   password: env.PASSWORD,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 // freezing
