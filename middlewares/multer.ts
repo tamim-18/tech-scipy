376d790e73fb20bf20de050917dcc30546add9de
tamim-18 <tamim11903060@gmail.com>
2024-08-07 19:10:29 +0600

    add multer middleware

---
+++ middlewares/multer.ts
@@ -0,0 +1,16 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary_config";
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // You can specify the folder name
+    format: async () => "png", // supports promises as well
+    public_id: (req: any, file: any) => file.filename,
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
